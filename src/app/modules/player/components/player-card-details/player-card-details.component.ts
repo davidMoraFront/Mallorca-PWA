92cea19eeb2aed54543a8fe4fe6442a60be3bcc7
David Mora <davidmora@MacBook-Pro-de-David.local>
2021-11-08 00:31:35 +0100

    change position stadistic

--- src/app/modules/player/components/player-card-details/player-card-details.component.ts
+++ src/app/modules/player/components/player-card-details/player-card-details.component.ts
@@ -44,11 +44,8 @@ export class PlayerCardDetailsComponent implements OnInit, AfterViewInit, OnDest
       this.player = res;
       this.dataStadistics = res.stadistics;
       this.reset();
-      
-      this.spinnerService.hide();
-      this.positionsList.length = 0;
-      this.dataStadistics.map(stad => this.positions(stad.name));
       this.fillForm(this.player);
+      this.spinnerService.hide();
     }));
   }
 
@@ -64,7 +61,7 @@ export class PlayerCardDetailsComponent implements OnInit, AfterViewInit, OnDest
     return this.fb.group({
       name: this.fb.control(el.name),
       value: this.fb.control(el.value),
-      position: this.fb.control(this.positionsList[index])
+      position: this.fb.control(this.positions(el.name))
     });
   }
 
@@ -107,9 +104,7 @@ export class PlayerCardDetailsComponent implements OnInit, AfterViewInit, OnDest
   positions(stadisticName: string) {
     this.playersSort = this.players.sort((a, b) => this.compare(a.stadistics.find(el => el.name === stadisticName ? el : '')!,
           b.stadistics.find(el => el.name === stadisticName ? el : '')!, Order.DES));
-    this.positionsList.push(this.playersSort.findIndex(player => player.id === this.player.id) + 1);
-    console.log(this.positionsList);
-    
+    return this.playersSort.findIndex(player => player.id === this.player.id) + 1;
   }
 
   compare(a: Stadistic, b: Stadistic, order: string): number {
